c992ea0653ff48b0684a33ec684770b165f8598c
baratahmed <baratahmed28@gmail.com>
2022-04-04 02:45:13 +0600

    first commit

---
+++ app/src/main/java/com/example/youtube_video_player/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.youtube_video_player;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+    Button btn;
+    YouTubePlayerView youTubePlayerView;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.play);
+        youTubePlayerView = findViewById(R.id.YoutubePlayerVideo);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("w9xfXsqIGKk");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                youTubePlayerView.initialize("AIzaSyBHUU0uYRcmeSLmTrZ3E9wlRk18DLykJIQ",onInitializedListener);
+            }
+        });
+    }
+}
